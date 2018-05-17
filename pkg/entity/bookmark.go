822ea9b12192cf862f17a4d69893bcd50470e1f9
Elton Minetto <eminetto@gmail.com>
2018-05-16 23:05:37 -0300

    Remove references to mongodb

--- pkg/entity/bookmark.go
+++ pkg/entity/bookmark.go
@@ -6,11 +6,11 @@ import (
 
 //Bookmark data
 type Bookmark struct {
-	ID          ID        `json:"id" bson:"_id"`
-	Name        string    `json:"name" bson:"name"`
-	Description string    `json:"description" bson:"description"`
-	Link        string    `json:"link" bson:"link"`
-	Tags        []string  `json:"tags" bson:"tags"`
-	Favorite    bool      `json:"favorite" bson:"favorite"`
-	CreatedAt   time.Time `json:"created_at" bson:"created_at"`
+	ID          ID        `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Link        string    `json:"link"`
+	Tags        []string  `json:"tags"`
+	Favorite    bool      `json:"favorite"`
+	CreatedAt   time.Time `json:"created_at"`
 }
