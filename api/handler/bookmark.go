b59b82f170d9eebd4478941c2e64eb528a5aa32b
Elton Minetto <eminetto@gmail.com>
2018-09-25 11:28:24 -0300

    add find and remove handlers

--- api/handler/bookmark.go
+++ api/handler/bookmark.go
@@ -72,13 +72,61 @@ func bookmarkAdd(service bookmark.UseCase) http.Handler {
 	})
 }
 
+func bookmarkFind(service bookmark.UseCase) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		errorMessage := "Error reading bookmark"
+		vars := mux.Vars(r)
+		id := vars["id"]
+		data, err := service.Find(entity.StringToID(id))
+		w.Header().Set("Content-Type", "application/json")
+		if err != nil && err != entity.ErrNotFound {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(errorMessage))
+			return
+		}
+
+		if data == nil {
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte(errorMessage))
+			return
+		}
+		if err := json.NewEncoder(w).Encode(data); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(errorMessage))
+		}
+	})
+}
+
+func bookmarkDelete(service bookmark.UseCase) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		errorMessage := "Error removing bookmark"
+		vars := mux.Vars(r)
+		id := vars["id"]
+		err := service.Delete(entity.StringToID(id))
+		w.Header().Set("Content-Type", "application/json")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(errorMessage))
+			return
+		}
+	})
+}
+
 //MakeBookmarkHandlers make url handlers
 func MakeBookmarkHandlers(r *mux.Router, n negroni.Negroni, service bookmark.UseCase) {
 	r.Handle("/v1/bookmark", n.With(
 		negroni.Wrap(bookmarkIndex(service)),
-	)).Methods("GET", "OPTIONS")
+	)).Methods("GET", "OPTIONS").Name("bookmarkIndex")
 
 	r.Handle("/v1/bookmark", n.With(
 		negroni.Wrap(bookmarkAdd(service)),
-	)).Methods("POST", "OPTIONS")
+	)).Methods("POST", "OPTIONS").Name("bookmarkAdd")
+
+	r.Handle("/v1/bookmark/{id}", n.With(
+		negroni.Wrap(bookmarkFind(service)),
+	)).Methods("GET", "OPTIONS").Name("bookmarkFind")
+
+	r.Handle("/v1/bookmark/{id}", n.With(
+		negroni.Wrap(bookmarkDelete(service)),
+	)).Methods("DELETE", "OPTIONS").Name("bookmarkDelete")
 }
--- api/handler/bookmark_test.go
+++ api/handler/bookmark_test.go
@@ -8,14 +8,22 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/codegangsta/negroni"
 	"github.com/eminetto/clean-architecture-go/pkg/bookmark"
 	"github.com/eminetto/clean-architecture-go/pkg/entity"
+	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestBookmarkIndex(t *testing.T) {
 	repo := bookmark.NewInmemRepository()
 	service := bookmark.NewService(repo)
+	r := mux.NewRouter()
+	n := negroni.New()
+	MakeBookmarkHandlers(r, *n, service)
+	path, err := r.GetRoute("bookmarkIndex").GetPathTemplate()
+	assert.Nil(t, err)
+	assert.Equal(t, "/v1/bookmark", path)
 	b := &entity.Bookmark{
 		Name:        "Elton Minetto",
 		Description: "Minetto's page",
@@ -62,6 +70,13 @@ func TestBookmarkSearch(t *testing.T) {
 func TestBookmarkAdd(t *testing.T) {
 	repo := bookmark.NewInmemRepository()
 	service := bookmark.NewService(repo)
+	r := mux.NewRouter()
+	n := negroni.New()
+	MakeBookmarkHandlers(r, *n, service)
+	path, err := r.GetRoute("bookmarkAdd").GetPathTemplate()
+	assert.Nil(t, err)
+	assert.Equal(t, "/v1/bookmark", path)
+
 	h := bookmarkAdd(service)
 
 	ts := httptest.NewServer(h)
@@ -83,5 +98,59 @@ func TestBookmarkAdd(t *testing.T) {
 	assert.True(t, entity.IsValidID(b.ID.String()))
 	assert.Equal(t, "http://github.com", b.Link)
 	assert.False(t, b.CreatedAt.IsZero())
+}
 
+func TestBookmarkFind(t *testing.T) {
+	repo := bookmark.NewInmemRepository()
+	service := bookmark.NewService(repo)
+	r := mux.NewRouter()
+	n := negroni.New()
+	MakeBookmarkHandlers(r, *n, service)
+	path, err := r.GetRoute("bookmarkFind").GetPathTemplate()
+	assert.Nil(t, err)
+	assert.Equal(t, "/v1/bookmark/{id}", path)
+	b := &entity.Bookmark{
+		Name:        "Elton Minetto",
+		Description: "Minetto's page",
+		Link:        "http://www.eltonminetto.net",
+		Tags:        []string{"golang", "php", "linux", "mac"},
+		Favorite:    true,
+	}
+	bID, _ := service.Store(b)
+	handler := bookmarkFind(service)
+	r.Handle("/v1/bookmark/{id}", handler)
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+	res, err := http.Get(ts.URL + "/v1/bookmark/" + bID.String())
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusOK, res.StatusCode)
+	var d *entity.Bookmark
+	json.NewDecoder(res.Body).Decode(&d)
+	assert.NotNil(t, d)
+	assert.Equal(t, bID, d.ID)
+}
+
+func TestBookmarkRemove(t *testing.T) {
+	repo := bookmark.NewInmemRepository()
+	service := bookmark.NewService(repo)
+	r := mux.NewRouter()
+	n := negroni.New()
+	MakeBookmarkHandlers(r, *n, service)
+	path, err := r.GetRoute("bookmarkDelete").GetPathTemplate()
+	assert.Nil(t, err)
+	assert.Equal(t, "/v1/bookmark/{id}", path)
+	b := &entity.Bookmark{
+		Name:        "Elton Minetto",
+		Description: "Minetto's page",
+		Link:        "http://www.eltonminetto.net",
+		Tags:        []string{"golang", "php", "linux", "mac"},
+		Favorite:    false,
+	}
+	bID, _ := service.Store(b)
+	handler := bookmarkDelete(service)
+	req, _ := http.NewRequest("DELETE", "/v1/bookmark/"+bID.String(), nil)
+	r.Handle("/v1/bookmark/{id}", handler).Methods("DELETE", "OPTIONS")
+	rr := httptest.NewRecorder()
+	r.ServeHTTP(rr, req)
+	assert.Equal(t, http.StatusOK, rr.Code)
 }
