5c039a56f0df9f83d5a608405f9e92deb45bb738
Elton Minetto <eminetto@gmail.com>
2020-03-10 09:21:59 -0300

    feat: ignore goland files

---
+++ pkg/middleware/metrics.go
@@ -0,0 +1,23 @@
+package middleware
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/thecodenation/highway/core/metric"
+
+	"github.com/codegangsta/negroni"
+)
+
+//Metrics to prometheus
+func Metrics(mService metric.UseCase) negroni.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+		appMetric := metric.NewHTTP(r.URL.Path, r.Method)
+		appMetric.Started()
+		next(w, r)
+		res := w.(negroni.ResponseWriter)
+		appMetric.Finished()
+		appMetric.StatusCode = strconv.Itoa(res.Status())
+		mService.SaveHTTP(appMetric)
+	}
+}
